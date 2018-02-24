dd32fc6626f4c4fd18c2ec5d9ee28cf5b547f0e6
Jason Gerard <jason-gerard>
2018-02-23 23:18:21 -0800

    added person twitter page to view

--- tweet_constructor.php
+++ tweet_constructor.php
@@ -1,10 +1,9 @@
 <?php
 
-require_once('simple_html_dom.php');
-
 $username = $_POST['username'];
 $tweet_text = $_POST['tweet-body'];
-$post_date = date('M j');
+
+require_once('simple_html_dom.php');
 
 $html = new simple_html_dom();
 
@@ -12,13 +11,25 @@ $profile_link = 'https://www.twitter.com/' . $username;
 
 $html->load_file($profile_link);
 
-foreach($html->find('.ProfileAvatar-image') as $image) {
-    $profile_image = $image->src;
-}
 
-foreach($html->find('.ProfileHeaderCard-name') as $name) {
-    $tweet_name = $name->first_child()->plaintext;
-}
+$profile_image = $html->find('.ProfileAvatar-image', 0)->src;
+$tweet_name = $html->find('.ProfileHeaderCard-name', 0)->first_child()->plaintext;
+$date = date('M j');
+
+$tweet_view = new simple_html_dom();
+
+$tweet_view->load_file('tweet_view.php');
+
+$tweet_view->find('.tweetEntry-avatar', 0)->src = $profile_image;
+$tweet_view->find('.tweetEntry-fullname', 0)->innertext = $tweet_name;
+$tweet_view->find('.tweetEntry-username', 0)->innertext = $username;
+$tweet_view->find('.tweetEntry-timestamp', 0)->innertext = $date;
+$tweet_view->find('.tweetEntry-text-container', 0)->innertext = $tweet_text;
+
+$tweet_list = $html->find('ol[class=stream-items]',0);
+$tweet_list->outertext = $tweet_view . $tweet_list->outertext;
+
+$doc = $html;
 
 ?>
 
@@ -31,17 +42,13 @@ foreach($html->find('.ProfileHeaderCard-name') as $name) {
 
     <title>Fake Tweet Generator</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="./assets/css/app.css">
     <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
 </head>
 <body>
-    <div class="container" style="margin-top: 150px">
-        <div class="row">
-            <?php
-                include('tweet_view.php');
-            ?>
-        </div>
-        <a style="margin-top: 150px; margin-left: 276px;" class="btn btn-primary" href="./">New Tweet</a>
-    </div>
+    <?php echo $doc ?>
+    <!-- <div class="div">
+        <a class="btn btn-primary" href="./">New Tweet</a>
+    </div> -->
 </body>
 </html>
--- tweet_view.php
+++ tweet_view.php
@@ -1,144 +1,3 @@
-<style>
-
-body {
-    font-family: Arial, sans-serif;
-    color: #292f33;
-}
-
-.tweetEntry-tweetHolder {
-    border-left: 1px solid #e1e8ed;
-    border-right: 1px solid #e1e8ed;
-    border-top: 1px solid #e1e8ed;
-    width: 588px;
-    margin: auto;
-}
-
-.tweetEntry-tweetHolder:hover {
-    background-color: #f5f8fa;
-}
-
-.tweetEntry-action-list {
-    display: block;
-    line-height: 18px;
-    margin-left: 58px;
-    margin-top: 10px;
-    color: #292f33;
-}
-
-.comment:hover {
-    color: #1DA1F2;
-    text-color: #1DA1F2;
-}
-
-.comment:hover .fa-comment {
-    color: #1DA1F2;
-    text-color: #1DA1F2;
-}
-
-.fa-retweet:hover {
-    color: #5cd65c;
-}
-
-.retweet:hover .fa-retweet {
-    color: #5cd65c;
-    text-color: #5cd65c;
-}
-
-.fa-heart:hover {
-    color: #ff3333;
-}
-
-.heart:hover .fa-heart {
-    color: #ff3333;
-    text-color: #ff3333;
-}
-
-.fa-envelope:hover {
-    color: #1DA1F2;
-}
-
-.fa-chevron-down:hover {
-    color: #1DA1F2;
-}
-
-.fa-chevron-down {
-    float: right;
-}
-
-.tweetEntry {
-    border-bottom: 1px solid #e1e8ed;
-    cursor: pointer;
-    min-height: 51px;
-    padding: 9px 12px;
-}
-
-.tweetEntry-content {
-    display: block;
-    font-size: 14px;
-    margin-left: 58px;
-    font-size: 14px;
-}
-
-.tweetEntry-account-group {
-    color: #8899a6;
-    padding: 0px;
-    text-decoration: none;
-    line-height: 20px;
-}
-
-.tweetEntry-avatar {
-    float: left;
-    height: 48px;
-    width: 48px;
-    margin-left: -58px;
-    border-radius: 50%;
-}
-
-.tweetEntry-fullname {
-    font-weight: bold;
-    font-size: 14px;
-    color: #292f33;
-    padding: 0px;
-}
-
-.tweetEntry-fullname:hover {
-    color: #1DA1F2;
-    text-decoration: underline;
-}
-
-.tweetEntry-username {
-    font-weight: normal;
-    font-size: 14px;
-    color: #657786;
-    padding: 0px;
-}
-
-.tweetEntry-timestamp {
-    font-size: 14px;
-    color: #657786;
-    padding: 0px;
-}
-
-.tweetEntry-timestamp::before {
-    content: "\00b7";
-}
-
-.tweetEntry-text-container {
-    word-wrap: break-word;
-    font-size: 14px;
-    color: #292f33;
-    line-height: 18px;
-}
-
-.action-num {
-    color: #657786;
-    font-size: 12px;
-    font-weight: bold;
-    line-height: 1;
-}
-
-</style>
-
 <div class="tweetEntry-tweetHolder">
 
     <div class="tweetEntry">
@@ -147,27 +6,22 @@ body {
 
             <div class="tweetEntry-account-group">
 
-                <img class="tweetEntry-avatar" src="<?php echo $profile_image ?>">
+                <img class="tweetEntry-avatar" src="">
 
-                <strong class="tweetEntry-fullname">
-                    <?php echo $tweet_name ?>
-                </strong>
+                <strong class="tweetEntry-fullname"></strong>
 
-                <span class="tweetEntry-username">
-                    @<?php echo $username ?>
-                </span>
+                <span class="tweetEntry-username"></span>
 
-                <span class="tweetEntry-timestamp">
-                    <?php echo $post_date ?>
-                </span>
+                <span class="tweetEntry-timestamp"></span>
 
-                <i class="fas fa-chevron-down"></i>
+                <div class="tweetEntry-pointers">
+                    <i class="fas fa-caret-down"></i>
+                    <i class="fas fa-chevron-down"></i>
+                </div>
 
             </div>
 
-            <div class="tweetEntry-text-container">
-                <?php echo $tweet_text ?>
-            </div>
+            <div class="tweetEntry-text-container"></div>
 
         </div>
 
